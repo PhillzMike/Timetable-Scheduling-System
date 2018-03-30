0f059b9d48f8ca1909c0e9b8f75e5db494b47453
Timilehin <phillzmike@gmail.com>
2018-03-30 08:01:08 +0100

    First version of graph

--- SchedulingSystem/Matrix.cs
+++ SchedulingSystem/Matrix.cs
@@ -7,7 +7,33 @@ using System.Threading.Tasks;
 namespace SchedulingSystem
 {
     //Timi
-    public class Grapgh<T>
+    public class Graph<T>
     {
+        private Dictionary<T, int> repVertex;
+        private int[,] adjacencyMatrix;
+        public Graph(List<T> vertices)
+        {
+            this.repVertex = new Dictionary<T, int>();
+            this.adjacencyMatrix = new int[vertices.Count, vertices.Count];
+            int i = 0;
+            foreach (T item in vertices)
+            {
+                this.repVertex.Add(item,i++);
+            }
+        }
+
+        public void SetEdge(T v, T v1)
+        {
+            this.adjacencyMatrix[repVertex[v], repVertex[v1]] = 1;
+        }
+        public void SetEdge(T v, T v1, int cost)
+        {
+            this.adjacencyMatrix[repVertex[v], repVertex[v1]] = cost;
+        }
+
+        public int GetEdgeCount(T v, T v1)
+        {
+            return this.adjacencyMatrix[repVertex[v], repVertex[v1]];
+        }
     }
 }
