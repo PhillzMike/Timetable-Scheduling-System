2cea7c03e2a82a6e0b4e154a46ef77959e58a599
Zaida-3dO <opsijay@gmail.com>
2018-04-10 18:27:54 +0100

    Seyi's work

--- SchedulingSystem/Lecturer.cs
+++ SchedulingSystem/Lecturer.cs
@@ -3,9 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace SchedulingSystem {
     //Seyi
     public class Lecturer {
+        private string name;
+        private List<Course> CourseTaken;
+
+        public Lecturer(string name, List<Course> CTaken) {
+            this.name = name;
+            this.CourseTaken = new List<Course>();
+            for (int i = 0; i < CTaken.Count; i++) {
+                CourseTaken.Add(CTaken[i]);
+            }
+        }
+
+        public string LecturerName {
+            get => name;
+        }
+        public List<Course> Courses {
+            get => new List<Course>(CourseTaken);
+        }
     }
 }
--- SchedulingSystem/Timetable.cs
+++ SchedulingSystem/Timetable.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace SchedulingSystem {
     //3dO
-    class Timetable {
+    public class Timetable {
+
     }
 }
