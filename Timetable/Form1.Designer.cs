a496f88552d8755c98303971573621eb60aae238
Zaida-3dO <opsijay@gmail.com>
2018-04-10 19:26:29 +0100

    Seyis work

--- Timetable/Form1.Designer.cs
+++ Timetable/Form1.Designer.cs
@@ -32,9 +32,9 @@
             this.ClientSize = new System.Drawing.Size(574, 300);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
+           
         }
 
         #endregion
