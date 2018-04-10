642c0fd13a74b87d1719803d3fce9632ba94be77
10ni <faithsulaiman@gmail.com>
2018-04-10 16:38:07 +0100

    venue class and excel reference

--- SchedulingSystem/InputClass.cs
+++ SchedulingSystem/InputClass.cs
@@ -8,6 +8,6 @@ namespace SchedulingSystem
 {
     //Teni
     public class Input
-    {
+    {mmm 
     }
 }
--- SchedulingSystem/Venue.cs
+++ SchedulingSystem/Venue.cs
@@ -3,9 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.Office.Interop.Excel;
 namespace SchedulingSystem {
     //Derry
     class Venue {
+        private string VenueName;
+        private int VenueCapacity;
+        private bool IsLab;
+
+        public Venue( String Name, int Capacity, bool Lab ){
+        this.VenueName = Name;
+        this.VenueCapacity = Capacity;
+        this.IsLab = Lab;
+        
+        
+}    
+        public string getName {get {  return this.VenueName;}}
+        
+        public int getCapacity{get {return this.VenueCapacity;}}
+
+         public bool isLab {get {return this.isLab;}}
+
+        
+
     }
 }
