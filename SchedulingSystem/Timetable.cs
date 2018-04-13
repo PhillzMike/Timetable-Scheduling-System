220e4e833610815701db633cbbe19ca4f7a62fe8
Zaida-3dO <opsijay@gmail.com>
2018-04-13 18:00:59 +0100

    K

--- SchedulingSystem/Timetable.cs
+++ SchedulingSystem/Timetable.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 namespace SchedulingSystem {
     //3dO
     public class Timetable {
-
+        public Timetable(string toExcel, Tuple<DateTime, DateTime> DailyTime) {
+            Input ngine = new Input(toExcel, DailyTime);
+            HashSet<Course> AllCourses = new HashSet<Course>(ngine.AllCourses.Values.ToList());
+            Graph ical = new Graph(ngine.AllCourses.Values.ToList());
+            foreach(Course c1 in )
+        }
     }
 }
