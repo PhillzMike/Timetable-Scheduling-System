57b34c8b0afc8c2a22f92fdeed6f267ffe817752
Zaida-3dO <opsijay@gmail.com>
2018-04-14 20:03:08 +0100

    Size

--- Timetable/Form1.Designer.cs
+++ Timetable/Form1.Designer.cs
@@ -29,12 +29,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(574, 300);
+            this.ClientSize = new System.Drawing.Size(574, 544);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
 
-           
         }
 
         #endregion
